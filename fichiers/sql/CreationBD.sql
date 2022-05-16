196ba8c47e3ac55956989a68d6d886c3f41d0bb7
Lysquid <romainbenoit21560@gmail.com>
2022-05-16 16:27:50 +0200

    update bd

--- fichiers/sql/CreationBD.sql
+++ fichiers/sql/CreationBD.sql
@@ -1,27 +1,27 @@
 drop table Seuil;
 drop table CodeBarre;
 drop table Produit;
-drop table TypeProduit;
+drop table CategorieProduit;
 drop table Mesure;
 drop table Capteur;
 drop table TypeMesure;
-drop table Ouverture;
-create table Ouverture(
-    idOuverture int(8) auto_increment,
+drop table OuverturePorte;
+create table OuverturePorte(
+    idOuverturePorte int(8) auto_increment,
     date DateTime not null,
-    ouvert boolean not null,
-    primary key (idOuverture)
+    porteOuverte boolean not null,
+    primary key (idOuverturePorte)
 );
 create table TypeMesure(
     idTypeMesure int(8) auto_increment,
-    typeMesure varchar(20) not null,
+    nomTypeMesure varchar(20) not null,
     unite varchar(20) not null,
     primary key (idTypeMesure)
 );
 create table Capteur(
     idCapteur int(8) auto_increment,
     idTypeMesure int(8) not null,
-    dateInstallation Date not null,
+    dateInstallation Date,
     primary key (idCapteur),
     foreign key (idTypeMesure) references TypeMesure(idTypeMesure)
 );
@@ -33,32 +33,33 @@ create table Mesure(
     primary key(idMesure),
     foreign key (idCapteur) references Capteur(idCapteur)
 );
-create table TypeProduit(
-    idTypeProduit int(8) auto_increment,
-    typeProduit varchar(20) not null,
-    primary key (idTypeProduit)
+create table CategorieProduit(
+    idCategorieProduit int(8) auto_increment,
+    nomCategorieProduit varchar(20) not null,
+    primary key (idCategorieProduit)
 );
 create table Produit(
     codeBarre int(13) not null,
-    idTypeProduit int(8),
-    quantite int,
+    idCategorieProduit int(8),
+    nomProduit varchar(20),
+    quantite int not null,
     primary key(codeBarre),
-    foreign key (idTypeProduit) references TypeProduit(idTypeProduit)
+    foreign key (idCategorieProduit) references CategorieProduit(idCategorieProduit)
 );
 create table CodeBarre(
     idCodeBarre int(8) auto_increment,
-    codeBarre int(13),
-    date DateTime,
-    ajout boolean,
+    codeBarre int(13) not null,
+    date DateTime not null,
+    ajout boolean not null,
     primary key (idCodeBarre),
     foreign key (codeBarre) references Produit(codeBarre)
 );
 create table Seuil(
-    idTypeProduit int(8),
+    idCategorieProduit int(8) not null,
     idTypeMesure int(8) not null,
-    seuilMin float not null,
-    seuilMax float not null,
-    primary key (idTypeProduit, idTypeMesure),
-    foreign key (idTypeProduit) references TypeProduit(idTypeProduit),
+    seuilMin float,
+    seuilMax float,
+    primary key (idCategorieProduit, idTypeMesure),
+    foreign key (idCategorieProduit) references CategorieProduit(idCategorieProduit),
     foreign key (idTypeMesure) references TypeMesure(idTypeMesure)
 );
