3d1f18b329a43f7678f3a6376520c3b5c9fc2326
deubn31 <debarge.nicolas@gmail.com>
2022-05-18 09:42:00 +0200

    nouvelles requêtes

--- fichiers/sql/Requetes.sql
+++ fichiers/sql/Requetes.sql
@@ -20,7 +20,7 @@ WHERE Seuil.nomTypeMesure = TypeMesure.nomTypeMesure
 AND Seuil.nomCategorieProduit = CategorieProduit.nomCategorieProduit
 AND TypeMesure.nomTypeMesure = ?;
 
--- Affichage de la quantité d'un produit correspondant à un code barre
+-- Affichage du produit et de sa quantité, correspondant à un code barre
 
 SELECT idCodebarre, nomProduit, quantite
 FROM CodeBarre, Produit
@@ -28,14 +28,35 @@ WHERE CodeBarre.codeBarre = Produit.codeBarre;
 
 -- pour chaque capteur, envoyer le type, la valeur et l’unité de la mesure sur un certaine période de temps
 
-SELECT Capteur.nomCapteur, nomTypeMesure, valeur, unite, mesure.date
+SELECT Capteur.nomCapteur, nomTypeMesure, valeur, unite, Mesure.dateMesure
 FROM TypeMesure, Capteur, Mesure
 WHERE TypeMesure.nomTypeMesure = Capteur.nomTypeMesure
 AND Capteur.nomCapteur = Mesure.nomCapteur
-AND date < ?
-AND date > ?;
+AND dateMesure < ?
+AND dateMesure > ?;
 
--- pour récupérer la liste des produits dans le réfrigérateur
+-- pour récupérer la liste des produits dans le réfrigérateur et leur quantité
+
+SELECT nomProduit, quantite
+FROM Produit
+WHERE quantite != 0;
+
+-- Nombre de produit ajouté dans le réfrigérateur pendant ? temps
+
+SELECT Count(nomProduit)*quantite as nbProduit
+FROM Produit, CodeBarre
+WHERE Produit.codeBarre = CodeBarre.codeBarre
+AND dateCodeBarre >= ?
+AND dateCodeBarre <= ?
+AND ajout = true;
+
+-- récupérer le nom des produits ajoutés pendant un certains temps
+SELECT nomProduit, dateCodeBarre
+FROM CodeBarre, Produit
+WHERE Produit.codeBarre = CodeBarre.codeBarre
+AND dateCodeBarre <= ?
+AND dateCodeBarre >= ?
+AND ajout = true;
 
 
 
