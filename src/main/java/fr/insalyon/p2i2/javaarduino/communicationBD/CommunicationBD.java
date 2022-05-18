6a46f43aa476b5b7b27ee59dc655aa64d3712005
Lysquid <romainbenoit21560@gmail.com>
2022-05-18 11:38:30 +0200

    fix

--- src/main/java/fr/insalyon/p2i2/javaarduino/communicationBD/CommunicationBD.java
+++ src/main/java/fr/insalyon/p2i2/javaarduino/communicationBD/CommunicationBD.java
@@ -3,7 +3,6 @@ package fr.insalyon.p2i2.javaarduino.communicationBD;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.math.BigInteger;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
